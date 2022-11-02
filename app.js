dc31741648d5b623ff6fb13d3e56f5d7fb809238
Ethan Swafford <ethanswafford@yahoo.com>
2022-11-02 12:17:56 -0500

    devPush

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // We’ ll hold the meal, price, and their respective getters and setters in an object named menu.
 // In app.js, create an empty menu object.
 
-let menu = {
+// The menu object will hold the meal and price of Today’s Special as properties and they shouldn’t be altered directly.
+// Within the menu object, create a _meal property with a value of an empty string('').
+// This will eventually hold the name of the meal.
 
+// Next, add a _price property with a value of 0. 
+// This will eventually hold the price of the meal, and should also not be altered directly.
+
+
+
+let menu = {
+    _meal: '',
+    _price: 0,
 }
 
 
