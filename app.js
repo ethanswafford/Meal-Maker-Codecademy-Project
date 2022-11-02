525c61b700386f8aa35234e4897fd0fe310d13e1
Ethan Swafford <ethanswafford@yahoo.com>
2022-11-02 11:16:53 -0500

    devPush

---
+++ app.js
@@ -0,0 +1,12 @@
+// We’ ll hold the meal, price, and their respective getters and setters in an object named menu.
+// In app.js, create an empty menu object.
+
+let menu = {
+
+}
+
+
+
+
+
+console.log("CONNECTED");
